f13f3ea863c0b7183cbc3a98c4e312a28ac170d3
davidLif <davidshani12@gmail.com>
2015-06-07 20:39:32 +0300

    flag english unicode

--- src/db_entities/Entity.java
+++ src/db_entities/Entity.java
@@ -1,5 +1,9 @@
 package db_entities;
 
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 
 public abstract class Entity implements java.io.Serializable{
@@ -9,6 +13,7 @@ public abstract class Entity implements java.io.Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	public String name;
+	public boolean valid_name = false;
 	public int class_id;
 	
 	public Entity(String name){
@@ -21,6 +26,16 @@ public abstract class Entity implements java.io.Serializable{
 	}
 
 	public void setName(String name) {
+		if (name == null){
+			this.name = null;
+			return;
+		}
+		String UTF8_str_eng = getEnglishUTF8(name);
+		if (!name.equals(UTF8_str_eng) || name.equals("")){
+			this.name = null;
+			return;
+		}
+		valid_name = true;
 		this.name = name;
 	}
 
@@ -30,6 +45,21 @@ public abstract class Entity implements java.io.Serializable{
 
 	public void setClass_id(int class_id) {
 		this.class_id = class_id;
+	}
+	
+	private static String getEnglishUTF8(String input_name){
+        try {
+            byte[] utf8Bytes = input_name.getBytes("UTF-8");
+
+            input_name = new String(utf8Bytes, "UTF-8");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Pattern unicodeOutliers = Pattern.compile("[^\\x00-\\x7F]",Pattern.UNICODE_CASE | Pattern.CANON_EQ| Pattern.CASE_INSENSITIVE);
+        Matcher unicodeOutlierMatcher = unicodeOutliers.matcher(input_name);
+
+        return unicodeOutlierMatcher.replaceAll("");
 	}
 	
 	
